e63eb93a2c80e9c279d4afd341d5fce1641b9c85
nedlarry <lrrchinedu@gmail.com>
2021-01-29 13:11:31 +0100

    added try

--- routes.js
+++ routes.js
@@ -17,62 +17,69 @@ routes.get('/', (req, res) => {
 
 routes.post('/validate-rule', (req, res) => {
 
-    const RULE_REQUIRED = ['field', 'condition', 'condition_value']
-    const PAYLOAD_REQUIERED = ['rule', 'data']
-
-    const payLoadKeys = Object.keys(req.body)
-    const PAYLOAD = req.body
-
-    const rules = {...PAYLOAD.rule}
-    const data = {...PAYLOAD.data}
-
-    if(typeof PAYLOAD == null || undefined){
-        return res.status(400).json({message: 'Invalid JSON Payload',status:'error', data:null})
-    }
-
-    Object.keys(PAYLOAD).forEach(field =>{
-
-        if (field == 'rule'){
-            if (typeof PAYLOAD[field] != 'object'){
-                res.status(400).json({message: `${field} should be an object.`, status: 'error'})
-            }
-        } 
-        if (field == 'data'){
-            if (typeof PAYLOAD[field] !== 'object' ){
-                res.status(400).json({message: `${field} should be an object.`, status: 'error'})
-            }
-        } 
-    })
-
-
-    PAYLOAD_REQUIERED.forEach(required => {
-        if (!payLoadKeys.includes(required)){
-            res.status(400).json({message: required +" is required.", status: 'error'})
+    try {
+        
+        const RULE_REQUIRED = ['field', 'condition', 'condition_value']
+        const PAYLOAD_REQUIERED = ['rule', 'data']
+    
+        const payLoadKeys = Object.keys(req.body)
+        const PAYLOAD = req.body
+    
+        const rules = {...PAYLOAD.rule}
+        const data = {...PAYLOAD.data}
+    
+        if(typeof PAYLOAD == null || undefined){
+            return res.status(400).json({message: 'Invalid JSON Payload',status:'error', data:null})
         }
-    })
-
-    if(typeof(rules) != 'object'){
-        res.status(400).json({message: "rule should be an object.", status: 'error'})
-    }
-
-    RULE_REQUIRED.forEach(required => {
-        if(!rules.hasOwnProperty(required)){
-            res.status(400).json({"message": required +" is required.", status: 'error'})
+    
+        Object.keys(PAYLOAD).forEach(field =>{
+    
+            if (field == 'rule'){
+                if (typeof PAYLOAD[field] != 'object'){
+                    res.status(400).json({message: `${field} should be an object.`, status: 'error'})
+                }
+            } 
+            if (field == 'data'){
+                if (typeof PAYLOAD[field] !== 'object' ){
+                    res.status(400).json({message: `${field} should be an object.`, status: 'error'})
+                }
+            } 
+        })
+    
+    
+        PAYLOAD_REQUIERED.forEach(required => {
+            if (!payLoadKeys.includes(required)){
+                res.status(400).json({message: required +" is required.", status: 'error'})
+            }
+        })
+    
+        if(typeof(rules) != 'object'){
+            res.status(400).json({message: "rule should be an object.", status: 'error'})
         }
-    })
+    
+        RULE_REQUIRED.forEach(required => {
+            if(!rules.hasOwnProperty(required)){
+                res.status(400).json({"message": required +" is required.", status: 'error'})
+            }
+        })
+    
+        if(!Object.keys(data).includes(rules.field)){
+            res.status(400).json({message: 'field ' + rules.field + ' is missing from data', status: 'error'})
+        }
+    
+        const validationObj = validationCheck(rules, data)
+        const validationReport = {validation: validationObj}
+    
+        if (validationObj.error){
+            res.status(400).json({message: `field ${rules.field} failed validation.`,status: 'error', data: validationReport})
+        }
+    
+        res.status(200).json({message:`field ${rules.field} successfully validated.`, status: 'success', data: validationReport })
 
-    if(!Object.keys(data).includes(rules.field)){
-        res.status(400).json({message: 'field ' + rules.field + ' is missing from data', status: 'error'})
+    } catch (error) {
+        res.status(400).json({error})
+        
     }
-
-    const validationObj = validationCheck(rules, data)
-    const validationReport = {validation: validationObj}
-
-    if (validationObj.error){
-        res.status(400).json({message: `field ${rules.field} failed validation.`,status: 'error', data: validationReport})
-    }
-
-    res.status(200).json({message:`field ${rules.field} successfully validated.`, status: 'success', data: validationReport })
 
 
 
